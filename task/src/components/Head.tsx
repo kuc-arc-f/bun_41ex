0e2de952ee670471230cabd20c658d2faf56c708
中島浩二 <knaka0209@gmail.com>
2025-08-13 18:22:40 +0900

    up, 0812a17

---
+++ task/src/components/Head.tsx
@@ -0,0 +1,13 @@
+//import { Routes, Route, Link } from 'react-router-dom';
+import {Link } from 'react-router-dom';
+
+function Page() {
+  return (
+  <div>
+    <a href="/" className="font-bold ms-4" > Home </a>
+    <a href="/task_project" className="font-bold ms-4" > [ Task ] </a>
+    <hr className="my-2" />
+  </div>
+  );
+}
+export default Page;
