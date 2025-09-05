9af6f58b561aec8d04fa20c089434c7071adbeb2
中島浩二 <knaka0209@gmail.com>
2025-09-05 19:27:31 +0900

    up, 0904a17

---
+++ headless-app-1/README.md
@@ -0,0 +1,39 @@
+﻿# headless-app-1
+
+ Version: 0.9.1
+
+ date    : 2025/09/03
+
+ update :
+
+***
+
+bun ,  headless CMS app example
+
+***
+* .env
+```
+EXTERNAL_API_URL="http://localhost:8787"
+API_KEY=123
+```
+***
+### headless CMS
+
+https://github.com/kuc-arc-f/headless-2025/tree/main/headless-2025
+
+***
+### build
+
+* build, dev-start
+
+```
+bun run build
+bun run dev
+```
+
+***
+### blog
+
+* 
+
+
---
+++ headless-app-1/src/index.ts
@@ -0,0 +1,37 @@
+
+import express from 'express';
+import { renderToString } from 'react-dom/server';
+
+const app = express();
+import 'dotenv/config'
+
+import Top from './pages/App';
+import todoRouter from './routes/todos';
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+console.log("env=", process.env.NODE_ENV)
+console.log("EXTERNAL_API_URL=", process.env.EXTERNAL_API_URL)
+//console.log(process.env);
+
+const errorObj = {ret: "NG", messase: "Error"};
+
+// API
+app.use('/api/todos', todoRouter);
+
+// SPA
+app.get('/*', (req: any, res: any) => {
+  try {
+    res.send(renderToString(Top()));
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+//start
+const PORT = 3000;
+app.listen({ port: PORT }, () => {
+  console.log(`Server ready at http://localhost:${PORT}`);
+});
+console.log('start');
